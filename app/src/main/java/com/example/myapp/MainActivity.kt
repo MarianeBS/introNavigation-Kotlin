124b0c9408470a07d0bf54c7be089e2218406767
MarianeBS <mariane.souza22@etec.sp.gov.br>
2023-08-30 14:07:34 -0300

    Reprodução de texto inserido em TextField

---
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+
+import com.example.myapp.telas.TaskScreen
+import com.example.myapp.ui.theme.MyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            //val navHostController = rememberNavController()
+
+            MyAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //MainScreen()
+                    TaskScreen()
+                }
+            }
+        }
+    }
+}
